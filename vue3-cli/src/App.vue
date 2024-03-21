33155e5389bfb5c33b3825c31d2a407e80f6fe41
mhhan <mun01180@gmail.com>
2024-03-21 15:24:48 +0900

    로그인폼 컴포지션

--- vue3-cli/src/App.vue
+++ vue3-cli/src/App.vue
@@ -1,33 +1,39 @@
-<!-- HTML -->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-  <hello-world msg="Welcome to Your Vue.js App"></hello-world>
+  <AppHeader 
+    v-bind:appTitle="message"
+    v-on:change="chageMessage">
+  
+  </AppHeader>
 </template>
 
-<!-- JavaScript -->
 <script>
-// import 라이브러리 from '라이브러리이름'
-// import 파일이름 from './파일경로'
-import HelloWorld from './components/HelloWorld.vue'
+// import 컴포넌트이름 from './컴포넌트 경로'
+import AppHeader from './components/AppHeader.vue'
 
 export default {
-  // 인스턴스 옵션 속성
-  name: 'App',
   components: {
-    HelloWorld
+    // '컴포넌트이름': 컴포넌트 내용
+    // 01.'app-header': AppHeader
+    // 02.'AppHeader': AppHeader
+    // 03.AppHeader: AppHeader
+    AppHeader
+  },
+  data() {
+    return {
+      message: '앱 헤더 컴포넌트'
+    }
+  },
+  methods: {
+    // showAlert() {
+    //   alert('hello');
+    // },
+    chageMessage() {
+      this.message = '변경됨'
+    }
   }
 }
 </script>
 
-<!-- CSS -->
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+
 </style>
